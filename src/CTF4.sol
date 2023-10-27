bf032ec467959bf17e7bfbef940e7506d198a8d2
brandon engstrom <bengstrom32@gmail.com>
2023-10-27 13:36:50 -0400

    first commit

--- src/CTF4.sol
+++ src/CTF4.sol
@@ -18,7 +18,7 @@ contract CTF4 is Isolution4 {
 
 
 
-    function solution(uint256 number2) public pure override returns (uint256) {
+    function solution(uint256 num) public pure override returns (uint256) {
         uint256 counter = 0;
         uint256 greatestPOW = 1;
         uint256 answer;
@@ -27,11 +27,11 @@ contract CTF4 is Isolution4 {
             // initialize loop variable i to 0
             let i := 0
 
-            // loop with a maximum iteration check as a fail-safe (in this case 256 to account for uint256's size)
+            // loop with a maximum iteration check at 256 to account for uint256 size)
             for { } lt(i, 256) { i := add(i, 1) }
             {
-                // Check the condition for greatestPOW
-                if gt(mul(greatestPOW, 2), number2) {
+                // check the condition for greatestPOW
+                if gt(mul(greatestPOW, 2), num) {
                     // exit the loop if the next doubling of greatestPOW would exceed number2
                     break
                 }
