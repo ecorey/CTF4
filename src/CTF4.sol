59207a108ad6831f5a6b1a509a5f25dd514d6f2f
brandon engstrom <bengstrom32@gmail.com>
2023-11-02 11:58:13 -0400

    another commit

--- src/CTF4.sol
+++ src/CTF4.sol
@@ -3,26 +3,34 @@
 pragma solidity 0.8.17;
 
 
-interface Isolution4 {
+interface Isolution {
     function solution(uint256 number) external pure returns (uint256);
 }
 
 // REDO FUNCTIONS 
-contract CTF4 is Isolution4 {
+contract CTF4 is Isolution {
 
+    // stdin: 1                     stdout: 1 or 2**0
+    // stdin: 10                    stdout: 8 or 2**3
+    // stdin: 21                    stdout: 16 or 2**4
+    // stdin: 2048                  stdout: 2048 or 2**11
+    // stdin: 9223372036854775808   stdout: 9223372036854775808 or 2**63
+    // stdin: 0xffffffff            stdout: 2147483648 or 0x80000000 or 2**31
 
+    function solution(uint256 number) external pure override returns (uint256) {
+        uint256 greatestPOW = 2;
 
-       function solution (uint256 number) external pure returns (uint256){
+        while (greatestPOW * 2 <= number) {
+            greatestPOW *= 2;
+        }
 
-         uint256 greatestPOW = 1;
+        // If the input number is 1, then the greatest power of 2 less than or equal to 1 is 2^0 = 1.
+        if (number == 1) {
+            greatestPOW = 1;
+        }
 
-         while (greatestPOW ** 2 <= number) {
-             greatestPOW *= 2;
-         }
-
-         return greatestPOW;
-     }
-
+        return greatestPOW;
+    }
 
 
 }
